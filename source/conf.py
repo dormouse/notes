d8b0ed47832f5f34418955d3e25bbd7d162d8653
Dormouse Young <dormouse.young@gmail.com>
2025-03-06 13:21:39 +0800

    modify copyright

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Dormouse Notes'
-copyright = '2021, Dormouse Young'
+copyright = '2021-2025, Dormouse Young'
 author = 'Dormouse Young'
 
 # The full version, including alpha/beta/rc tags
